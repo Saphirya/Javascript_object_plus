a674d3a8911662cb8be358b7cc002deeabb62d7b
Florian <floriangeorges1992@gmail.com>
2024-12-20 17:29:06 +0100

    add property writable/enumerable/configurable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+const obj = {
+  key: "value",
+};
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+
+Object.defineProperty(obj, "test", {});
+
+// Object.defineProperty(obj, "key", {
+//   writable: true,
+// });
+
+const descriptor = Object.getOwnPropertyDescriptor(obj, "test");
+console.log(descriptor);
+
+// for (let key in obj) {
+//   console.log(key);
+// }
+
+obj.key = "value2";
+
+// {
+//   "value": "value2",
+//   "writable": true,
+//   "enumerable": true,
+//   "configurable": true
+// }
